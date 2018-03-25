f5af7dcadf02a24785f1b042b95adabea30e814e
Danilo <kimhodori@gmail.com>
2018-03-24 22:55:18 -0700

    adding a profile photo with css

--- myportfolio/src/components/landingpage.js
+++ myportfolio/src/components/landingpage.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-
+import { Grid, Cell } from 'react-mdl';
 class Landing extends Component {
   render() {
-    return (
-      <div>
-        <h1>Landing Page</h1>
-      </div>
-    );
+    return <div style={{ width: "100", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img src="https://avatars1.githubusercontent.com/u/18688674?s=400&u=61e8be77ad6a27be82cb3650beb6b08278a76e2b&v=4" 
+            alt="cafetero"
+            className="avatar-img"
+            />
+          </Cell>
+        </Grid>
+      </div>;
   }
 }
 
