6b4f4e137a9ebe999949cd89f2b91c17f01dd532
Danilo <kimhodori@gmail.com>
2018-03-27 22:41:22 -0700

    contact looks 💩... but oh well its working

--- myportfolio/src/components/contact.js
+++ myportfolio/src/components/contact.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import { Grid, Cell} from "react-mdl";
 
 class Contact extends Component {
   render() {
-    return (
-      <div>
-        <h1>Contact Page</h1>
-      </div>
-    );
+    return <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+          <h2>Danilo Kim</h2>
+          <img 
+            src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/13680683_10153788934593568_6210275365148678664_n.jpg?_nc_cat=0&oh=6ec4f1becc0d9b5d7618a46246e462b7&oe=5B7073F4"
+            alt="danilo in Guanujuato"
+            style={{height: '250px'}}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Eu veniam ex nulla quis.</p>
+          </Cell>
+          <Cell col={6}>Half Page</Cell>
+        </Grid>
+      </div>;
   }
 }
 
