639015b038f13d54a43dc4898e8c0faf7e389176
Danilo Kim <kimhodori@gmail.com>
2018-04-09 23:20:41 -0700

    Designing resume section

--- myportfolio/src/components/resume.js
+++ myportfolio/src/components/resume.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Cell} from 'react-mdl';
 
 class Resume extends Component {
      render() {
           return(
-               <div><h1>Resume Page</h1></div>
+               <div>
+               <Grid>
+                    <Cell col={4}>
+                         <div style={{textAlign: 'center'}}>
+                         <img src="https://media.licdn.com/dms/image/C5603AQGcElUXLo6-aw/profile-displayphoto-shrink_200_200/0?e=1528527600&v=beta&t=Lbh0buJhEyT1yIFB0u7g08roZWdY0fWvFQRfDkut9JM" alt="avatar" style={{height: '200px'}} 
+                         />
+                         </div>
+                         <h2 style={{paddingTop: '2em'}}>Danilo Kim</h2>
+                         <h4 style={{color: 'grey'}}>Full Stack Web Developer</h4>
+                         <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+
+                    </Cell>
+                    <Cell className="resume-right-col" col={8}>Right Side</Cell>
+               </Grid>
+               </div>
           )
      }
 }
