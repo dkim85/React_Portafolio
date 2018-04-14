d12570906c781e73ed2fce6ce12ca14a68b08828
Danilo <kimhodori@gmail.com>
2018-04-14 01:16:00 -0700

    design landing page

--- myportfolio/src/components/contact.js
+++ myportfolio/src/components/contact.js
@@ -9,7 +9,7 @@ class Contact extends Component {
             <h2>Danilo Kim</h2>
             <img src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/13680683_10153788934593568_6210275365148678664_n.jpg?_nc_cat=0&oh=6ec4f1becc0d9b5d7618a46246e462b7&oe=5B7073F4" alt="danilo in Guanujuato" style={{ height: "250px" }} />
             <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
-              Eu veniam ex nulla quis.
+              Me, @ Pipila, Guanuajuato , MX
             </p>
           </Cell>
           <Cell col={6}>
